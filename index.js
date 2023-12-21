6d5b0f954c2e306b6921e5a9778a9d0a9085fa89
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-12-21 22:03:30 +0000

    Added a blink animation (part 2)

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
     let poemElement = document.querySelector("#generator-result");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `Generating poem anout  ${instructionsInput.value}`
+    poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionsInput.value}</div>`
 
     console.log("Generating poem...")
     console.log(`Prompt: ${apiPrompt}`);
